5d2074d6c441fc9322ae0e318ac7587f881596c8
r4spb3rr1 <maga13105@gmail.com>
2024-01-30 13:39:13 +0600

    first commit

---
+++ tsis1/tsis1.py
@@ -0,0 +1,152 @@
+#example №1
+print("Hello, world!")
+
+
+#example №2
+#comment
+
+
+#example №3
+"""This is multiple 
+docstringssssss"""
+
+
+#example №4
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+#example №5
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+#example №6
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+#example №7
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#example №8
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#example №9
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#example №10
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#example №11
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#example №12
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+
+#example №13
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+#example №14
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+
+#example №15
+a = "Hello, World!"
+print(a[1])
+
+
+#example №16
+b = "Hello, World!"
+print(b[2:5])
+
+
+#example №17
+a = " Hello, World! "
+print(a.strip())
+
+
+#example №18
+a = "Hello, World!"
+print(len(a))
+
+
+#example №19
+a = "Hello, World!"
+print(a.lower())
+
+
+#example №20
+a = "Hello, World!"
+print(a.upper())
+
+
+#example №21
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+#example №22
+a = "Hello, World!"
+b = a.split(",")
+print(b)
